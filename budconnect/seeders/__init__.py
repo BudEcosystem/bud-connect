b1b67cb24f351b1aa32d0c184c929fc6415e00df
dittops <dittops@gmail.com>
2025-09-19 05:15:24 +0000

    fix: disable model seeder

--- budconnect/seeders/__init__.py
+++ budconnect/seeders/__init__.py
@@ -1,9 +1,10 @@
 from .engine import EngineSeeder
 from .guardrails import GuardrailsSeeder
 from .license import LicenseSeeder
-from .litellm import LiteLLMSeeder as LiteLLMSeeder
-from .model_details import ModelDetailsSeeder
-from .tensorzero import TensorZeroSeeder
+
+# from .litellm import LiteLLMSeeder as LiteLLMSeeder
+# from .model_details import ModelDetailsSeeder
+# from .tensorzero import TensorZeroSeeder
 from .user import UserSeeder
 
 
@@ -12,7 +13,7 @@ seeders = {
     "engine": EngineSeeder,
     "license": LicenseSeeder,  # This should run before litellm and tensorzero
     # "litellm": LiteLLMSeeder,
-    "tensorzero": TensorZeroSeeder,
-    "model_details": ModelDetailsSeeder,  # This should run after litellm and tensorzero
+    # "tensorzero": TensorZeroSeeder,
+    # "model_details": ModelDetailsSeeder,  # This should run after litellm and tensorzero
     "guardrails": GuardrailsSeeder,  # This should run after tensorzero to ensure providers exist
 }
